98af7e91a946c1228f2ab33b38e89bef0f94aaa7
tanaka <lemo.nade.engineer.room@gmail.com>
2022-06-24 04:07:40 +0900

    sessionに20分で削除機能を追加

--- src/session/storage/sessionStorage.ts
+++ src/session/storage/sessionStorage.ts
@@ -3,18 +3,23 @@ import { LSessions } from "../entity/lSessions.ts"
 export class SessionStorage {
 
     public constructor(
-        private readonly sessions: Record<string, Record<string, string>> = {}
+        private readonly sessions: Record<string, { last: number, storage: Record<string, string> }> = {}
     ) {}
+
+    private readonly timeLimit = 20 * 60 * 1000
 
     public readonly create = (): string => {
         const uuid = crypto.randomUUID()
-        this.sessions[uuid] = {}
+        this.sessions[uuid] = { last: Date.now(), storage: {} }
+        this.timer(uuid)
         return uuid
     }
 
     public readonly get = (uuid: string | null): LSessions => {
         const key = this.isExist(uuid) ? uuid! : this.create()
-        const storage = this.sessions[key]
+        this.sessions[key].last = Date.now()
+        const storage = this.sessions[key].storage
+        this.timer(key)
         return new LSessions(key, storage)
     }
 
@@ -22,5 +27,14 @@ export class SessionStorage {
         if (uuid === null) return false
         if (this.sessions[uuid] === undefined) return false
         return true
+    }
+
+    private readonly timer = (key: string) => {
+        setTimeout(() => {
+            if (!this.sessions[key]) return
+            if (Date.now() - this.sessions[key].last > this.timeLimit) {
+                delete this.sessions[key]
+            }
+        }, this.timeLimit * 1.2)
     }
 }
